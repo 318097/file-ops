8518cab0334761fa4e9e1ae239ecd54f7c764659
Mehul Lakhanpal <318097@gmail.com>
2020-04-21 09:56:35 +0530

    WIP: Add Tag

--- extension.js
+++ extension.js
@@ -1,30 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const fs = require("fs");
+const fsPromise = fs.promises;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const path = "./file-tag.json";
+
+const getData = async () => {
+  if (!fs.existsSync(path)) return {};
+  const data = await fsPromise.readFile(path, { encoding: "utf8" });
+  return JSON.parse(data) || {};
+};
+
+const saveData = async (data = {}) => {
+  const result = await fsPromise.writeFile(
+    path,
+    JSON.stringify(data, undefined, 2)
+  );
+  console.log("result::", result);
+  return result;
+};
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log('Congratulations, your extension "file-tag" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-tag" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "file-tag.createTag",
+    async () => {
+      try {
+        const input = await vscode.window.showInputBox({
+          placeHolder: "Enter description"
+        });
+        const activeTE = vscode.window.activeTextEditor;
+        const filePath = activeTE["_documentData"]["_uri"]["path"];
+        const data = await getData();
+        data[filePath] = input;
+        saveData(data);
+        // console.log(input, activeTE, filePath);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-tag.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from file-tag!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -32,6 +52,6 @@ exports.activate = activate;
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
